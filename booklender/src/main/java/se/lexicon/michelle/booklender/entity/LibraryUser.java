50a5d01ec9149e1f0810dbba4f85de495dff49d9
michelle <michelle.johansson85@gmail.com>
2020-09-21 10:14:05 +0200

    * added a full constructor to LibraryUser

--- booklender/src/main/java/se/lexicon/michelle/booklender/entity/LibraryUser.java
+++ booklender/src/main/java/se/lexicon/michelle/booklender/entity/LibraryUser.java
@@ -24,6 +24,13 @@ public class LibraryUser {
         this.email = email;
     }
 
+    public LibraryUser(int userId, LocalDate regDate, String name, String email) {
+        this.userId = userId;
+        this.regDate = regDate;
+        this.name = name;
+        this.email = email;
+    }
+
     public int getUserId() {
         return userId;
     }
